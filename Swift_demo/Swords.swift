7a1f9056b5936f783cd0b95fc9a26a6dd1b9ab31
florije1988 <florije1988@gmail.com>
2015-06-10 20:49:07 +0800

    不知道怎么用呀，就先这么写上的说。稍后进行处理的。

--- Swift_demo/Swords.swift
+++ Swift_demo/Swords.swift
@@ -27,3 +27,40 @@ func -(left: Sword, right: Sword) -> Sword {
     return Sword(length: left.length - right.length, weight: left.weight - right.weight, name: "Small Sword")
 }
 
+prefix func -(sword: Sword) -> Sword {
+    return Sword(length: -sword.length, weight: -sword.weight, name: sword.name)
+}
+
+postfix func ++(inout sword: Sword) -> Sword {
+    var oldSword = sword
+    sword.length += 1.0
+    sword.weight += 1.0
+    return oldSword
+}
+
+func ==(left: Sword, right: Sword) -> Bool {
+    return left.length == right.length && left.weight == right.weight
+}
+
+var smallSword1 = Sword(length: 0.5, weight: 0.5, name: "Small Sword1")
+var smallSword2 = Sword(length: 1.0, weight: 1.0, name: "Small Sword2")
+
+var bigSword = smallSword1 + smallSword2
+
+smallSword1.printDescription()
+smallSword2.printDescription()
+
+bigSword.printDescription()
+
+var smallSword3 = bigSword - smallSword2
+
+smallSword3.printDescription()
+
+var newSword = smallSword3++
+smallSword3.printDescription()
+newSword.printDescription()
+
+(-smallSword2).printDescription()
+
+println("newSword equals to smallSword1? \({newSword == smallSword1 ? true: false}())")
+println("newSword3 equals to smallSword1? \({newSword3 == smallSword1 ? true: false}())")
